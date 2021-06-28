9dcf96c5de05702725948e5cd41596e8bafcf7fc
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2021-06-28 18:46:34 +0200

    Finished tutorial - only Service not working due to some bean / import issue

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -1,5 +1,7 @@
 package com.example.testingweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,14 +9,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
-    private final GreetingServce service;
+
+    private final GreetingService service;
 
     public GreetingController(GreetingService service){
+
         this.service = service;
     }
 
     @RequestMapping(value="/greeting", method= RequestMethod.GET)
     public @ResponseBody String greeting() {
+
         return service.greet();
     }
 }
--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -1,4 +1,11 @@
 package com.example.testingweb;
 
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class GreetingService {
+    public String greet(){
+        return "Hello, World";
+    }
 }
