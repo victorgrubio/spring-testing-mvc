e0450a203ceec85bce823353bd55c8a9a126e388
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2021-06-29 19:39:46 +0200

    Fixed error in tests. Added github actions

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -1,25 +1,22 @@
 package com.example.testingweb;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class GreetingController {
 
     private final GreetingService service;
 
-    public GreetingController(GreetingService service){
-
+    public GreetingController(GreetingService service) {
         this.service = service;
     }
 
-    @RequestMapping(value="/greeting", method= RequestMethod.GET)
+    @RequestMapping("/greeting")
     public @ResponseBody String greeting() {
-
         return service.greet();
     }
+
 }
--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -2,10 +2,9 @@ package com.example.testingweb;
 
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class GreetingService {
-    public String greet(){
+    public String greet() {
         return "Hello, World";
     }
 }
